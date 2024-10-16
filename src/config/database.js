94484922ade812dadf038bd48187bbcb0a1d2341
Aman <aman30t@gmail.com>
2024-10-16 09:43:51 +0530

    done

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    console.log('MongoDB connected successfully');
+    console.log(`MongoDB connected successfully ${process.env.MONGO}`);
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
     process.exit(1); 
